7651cec33f6e0a16775fef62cccc6f9795fd09c8
Fedor_Kibanov <c-280d@mail.ru>
2022-10-21 16:58:42 +0500

    dz_5

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -24,3 +24,48 @@ reduce
 __mul__
 __add__
 """
+import collections
+import functools
+
+
+def hex_sum_mult():
+    nums = collections.defaultdict(list)
+
+    for d in range(2):
+        n = input(f'Введите {d + 1}-е натуральное шестнадцатеричное число: ')
+        nums[f'{d + 1}'] = list(n)
+
+    sum_res = sum([int(''.join(i), 16) for i in nums.values()])
+    print('Сумма: ', list("%X" % sum_res))
+
+    mul_res = functools.reduce(lambda a, b: a * b,
+                               [int(''.join(i), 16) for i in nums.values()])
+    print("Произведение: ", list('%X' % mul_res))
+
+
+hex_sum_mult()
+
+
+class HexSumMult:
+    def __init__(self, first_num, second_num):
+        self.first_num = first_num
+        self.second_num = second_num
+
+    def __add__(self, other):
+        return list(hex(int(''.join(self.first_num), 16)
+                        + int(''.join(other.second_num), 16)))[2:]
+
+    def __mul__(self, other):
+        return list(hex(int(''.join(self.first_num), 16)
+                        * int(''.join(other.second_num), 16)))[2:]
+
+
+hex_first_num = list(input('Введите первое число: '))
+hex_second_num = list(input('Введите второе число: '))
+
+res_sum = HexSumMult(hex_first_num, hex_second_num) \
+          + HexSumMult(hex_first_num, hex_second_num)
+res_mul = HexSumMult(hex_first_num, hex_second_num) \
+          * HexSumMult(hex_first_num, hex_second_num)
+print(f'Сумма чисеп = {res_sum}\nПроизведение чисел ={res_mul}')
+
--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -25,3 +25,131 @@ appendleft, popleft, extendleft дека и соответствующих им 
 для того, чтобы снизить погрешность, желательно операции по каждой ф-ции
 (append, pop и т.д.) проводить в циклах. Для замеров используйте timeit.
 """
+from collections import deque
+from timeit import timeit
+
+my_list = []
+my_deque = deque()
+
+
+# 1. Сравниваем быстродействие операций append, pop, extend
+def list_append_test(n):
+    for i in range(n):
+        my_list.append(n)
+    return my_list
+
+
+def deque_append_test(n):
+    for i in range(n):
+        my_deque.append(n)
+    return my_deque
+
+
+def list_pop_test(n):
+    for i in range(n):
+        my_list.pop()
+    return my_list
+
+
+def deque_pop_test(n):
+    for i in range(n):
+        my_deque.pop()
+    return my_list
+
+
+def list_extend_test(n):
+    for i in range(n):
+        my_list.extend([i for i in range(10)])
+    return my_list
+
+
+def deque_extend_test(n):
+    for i in range(n):
+        my_deque.extend([i for i in range(10)])
+    return my_list
+
+
+"""
+Быстродействие list и deque в опреациях append, pop и extend
+примерно одинаковое.
+"""
+
+
+# 2. Сравниваем быстродействие операций appendleft, popleft, extendleft
+def list_appendleft_test(n):
+    for i in range(n):
+        my_list.insert(0, n)
+    return my_list
+
+
+def deque_appendleft_test(n):
+    for i in range(n):
+        my_deque.appendleft(n)
+    return my_deque
+
+
+def list_popleft_test(n):
+    for i in range(n):
+        my_list.pop(0)
+    return my_list
+
+
+def deque_popleft_test(n):
+    for i in range(n):
+        my_deque.popleft()
+    return my_list
+
+
+def list_extendleft_test(n):
+    for i in range(n):
+        my_list.insert(0, [i for i in range(10)])
+    return my_list
+
+
+def deque_extendleft_test(n):
+    for i in range(n):
+        my_deque.extendleft([i for i in range(10)])
+    return my_list
+
+
+"""
+Быстродействие deque в операциях вставки с левого края массива
+на порядки выше, чеи у list.
+"""
+
+
+# 3. Сравниваем операции получения элемента по индексу в list и deque.
+def list_getitem_test(n):
+    for i in range(n):
+        my_list[i] = i
+    return my_list
+
+
+def deque_getitem_test(n):
+    for i in range(n):
+        my_deque[i] = i
+    return my_deque
+
+
+"""
+Быстродействие list в опреациях взятия элемента по индексу немного 
+лучше, чем у deque.
+"""
+
+
+if __name__ == '__main__':
+    print('list append: ', (timeit('list_append_test(1000)', globals=globals(), number=100)))
+    print('deque append: ', timeit('deque_append_test(1000)', globals=globals(), number=100))
+    print('list pop: ', timeit('list_pop_test(1000)', globals=globals(), number=100))
+    print('deque pop: ', timeit('deque_pop_test(1000)', globals=globals(), number=100))
+    print('list extend: ', timeit('list_extend_test(1000)', globals=globals(), number=100))
+    print('deque extend: ', timeit('deque_extend_test(1000)', globals=globals(), number=100))
+    print('list "appendleft": ', (timeit('list_appendleft_test(1000)', globals=globals(), number=10)))
+    print('deque appendleft: ', timeit('deque_appendleft_test(1000)', globals=globals(), number=10))
+    print('list "popleft": ', timeit('list_popleft_test(1000)', globals=globals(), number=10))
+    print('deque popleft: ', timeit('deque_popleft_test(1000)', globals=globals(), number=10))
+    print('list "extendleft": ', timeit('list_extendleft_test(1000)', globals=globals(), number=10))
+    print('deque extendleft: ', timeit('deque_extendleft_test(1000)', globals=globals(), number=10))
+    print('list get item: ', timeit('list_getitem_test(1000)', globals=globals(), number=10000))
+    print('deque get item: ', timeit('deque_getitem_test(1000)', globals=globals(), number=10000))
+
